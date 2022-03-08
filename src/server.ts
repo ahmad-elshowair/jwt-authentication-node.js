7dfe15962f2d2ae1df0881bbfccdb14cbdad5d70
Ahmad Elshowair <it.is.work@outlook.com>
2022-03-09 00:05:13 +0700

    start with basic server

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,18 @@
-console.log("hello here !");
+import express, { Request, Response, Application } from 'express';
+
+// initialize port variable
+const PORT = 8000;
+
+// declare an instance of express
+const app: Application = express();
+
+// add get request with the route /
+app.get('/', (req: Request, res: Response) => {
+    res.json({ message: 'Hey There !' });
+});
+
+// start the express server
+
+app.listen(PORT, () => {
+    console.log(`the server works on http://localhost:${PORT}`);
+});
